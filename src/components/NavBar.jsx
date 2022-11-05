ed6242f32961c8676b3f93b29827350adea155e6
yebinge <yebing@student.unimelb.edu.au>
2022-11-05 16:49:14 +1100

    finish NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
+import { navItems } from "../constants";
 
 export default function NavBar() {
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <Box sx={{ display: "flex" }}>
+      <AppBar component="nav">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Ethan Ge
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: "#fff" }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <Button sx={{ color: "#fff", pl: "40px" }}>Contact me</Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const navItems = ["Home", "Experience", "Portfolio", "Testimonial"];
