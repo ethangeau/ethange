75e63080b4d04e279e9dd05ebf401bab13dee383
yebinge <yebing@student.unimelb.edu.au>
2022-11-06 12:08:52 +1100

    add projects to constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,19 @@
 export const navItems = ["Home", "Experience", "Portfolio", "Testimonial"];
+export const projects = [
+  {
+    title: "Local Restaurants & Hotels Discovery",
+    imageUrl: "",
+    techStacks: ["React", "Google Map API"],
+    description:
+      "A web application that allows users to search for restaurants and hotels in their local area.",
+    website: "https://yebinge.github.io/local-food-shelter/",
+  },
+  {
+    title: "Melbourne Moments Sharing Board",
+    imageUrl: "",
+    techStacks: ["React", "Redux", "Express", "MongoDB"],
+    description:
+      "A web application that allows users to share their moments in Melbourne.",
+    website: "https://yebinge.github.io/melmoment-frontend/",
+  },
+];
