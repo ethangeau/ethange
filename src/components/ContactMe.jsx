57bc2c6b18405b26e00468986cdf79e62faf5fd8
yebinge <yebing@student.unimelb.edu.au>
2022-11-25 22:56:54 +1100

    finish ContactMe

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { Typography } from "@mui/material";
 
 export default function ContactMe() {
   return (
-    <div>
-      <h1>Contact Me</h1>
-    </div>
+    <>
+      <hr style={{ marginTop: 100 }} />
+      <Typography
+        variant="h4"
+        gutterBottom
+        sx={{ ml: 5, mt: 12, color: "#0014FF" }}
+      >
+        Contact Me
+      </Typography>
+      <Typography
+        variant="h6"
+        gutterBottom
+        sx={{
+          mx: 20,
+          my: 10,
+          color: "#1976d2",
+          letterSpacing: 1.5,
+          lineHeight: 2,
+        }}
+      >
+        If you are looking for a software engineer, please{" "}
+        <a href="mailto: geyebin0701@gmail.com">Email Me</a> or contact me via{" "}
+        <a href="https://www.linkedin.com/in/ethange/">Linkedin</a> and{" "}
+        <a href="https://github.com/yebinge">Github</a>.
+      </Typography>
+    </>
   );
 }
