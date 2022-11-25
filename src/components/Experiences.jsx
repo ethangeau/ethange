e29f9517a1c3d653579bad3dc681118f146f7a73
yebinge <yebing@student.unimelb.edu.au>
2022-11-25 20:46:52 +1100

    add Experiences component

---
+++ src/components/Experiences.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+import { experiences } from "../constants";
+import Experience from "./Experience";
+
+export default function Experiences() {
+  return (
+    <>
+      <hr style={{ marginTop: 100 }} />
+      <Typography
+        variant="h4"
+        gutterBottom
+        sx={{ ml: 5, mt: 12, color: "#0014FF" }}
+      >
+        Experience
+      </Typography>
+      <Grid container>
+        {experiences.map((item) => (
+          <Experience
+            key={item.id}
+            title={item.title}
+            company={item.company}
+            timeRanges={item.timeRanges}
+            location={item.location}
+            description={item.description}
+          />
+        ))}
+      </Grid>
+    </>
+  );
+}
