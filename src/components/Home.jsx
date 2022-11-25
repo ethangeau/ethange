b3ef79fedc8b6c8654408b0a0ccf1e2134c86ed8
yebinge <yebing@student.unimelb.edu.au>
2022-11-25 16:54:44 +1100

    modify Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,54 +7,91 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Home() {
   return (
-    <div>
-      <Grid container>
-        <Grid item xs={12} sm={4} align="center">
-          <Avatar
-            alt="Ethan Ge"
-            src={avatar}
-            sx={{ width: 150, height: 150 }}
-          />
-          <div style={{ padding: "20px" }}>
-            <a
-              href="https://github.com/yebinge"
-              target="_blank"
-              rel="noreferrer"
-              style={{ paddingRight: "20px" }}
-            >
-              <GitHubIcon fontSize="large" />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/ethan-g-795aa0217/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <LinkedInIcon fontSize="large" color="primary" />
-            </a>
-          </div>
-          <Typography variant="h4" gutterBottom sx={{ mt: 3 }}>
-            A brief introduction of myself
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={8}>
-          <Typography variant="h4" gutterBottom sx={{ mt: 5, ml: 5 }}>
-            Skills
-          </Typography>
-          <Typography variant="h5" gutterBottom sx={{ mt: 5, ml: 5 }}>
-            Programming Languages: JavaScript | Python | Java | HTML5 | CSS3 |
-            SQL | Shell
-          </Typography>
-          <Typography variant="h5" gutterBottom sx={{ mt: 2, ml: 5 }}>
-            Database: MySQL | MongoDB | CouchDB
-          </Typography>
-          <Typography variant="h5" gutterBottom sx={{ mt: 2, ml: 5 }}>
-            Frameworks: React | Bootstrap | Express | Django
-          </Typography>
-          <Typography variant="h5" gutterBottom sx={{ mt: 2, ml: 5 }}>
-            Developer Tools: Git | Linux | Ansible | Docker | AWS | Postman
-          </Typography>
-        </Grid>
+    <Grid container>
+      <Grid item align="center">
+        <Avatar
+          alt="Ethan Ge"
+          src={avatar}
+          sx={{ width: 150, height: 150, mt: 10 }}
+        />
+        <div style={{ padding: "20px" }}>
+          <a
+            href="https://github.com/yebinge"
+            target="_blank"
+            rel="noreferrer"
+            style={{ paddingRight: "20px" }}
+          >
+            <GitHubIcon fontSize="large" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/ethan-g-795aa0217/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <LinkedInIcon fontSize="large" color="primary" />
+          </a>
+        </div>
+        <Typography
+          variant="h3"
+          gutterBottom
+          sx={{
+            mx: 12,
+            mt: 8,
+            mb: 12,
+            color: "primary.main",
+            fontFamily: "Helvetica Neue",
+            lineHeight: 1.5,
+          }}
+        >
+          I am <span style={{ color: "#0014FF" }}>Ethan Ge</span>, I am
+          passionate about developing web applications and striving to create
+          innovative products.
+        </Typography>
       </Grid>
-    </div>
+      <Grid item xs={12}>
+        <hr />
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{ ml: 5, mt: 12, color: "#0014FF" }}
+        >
+          Skills
+        </Typography>
+        <div style={{ color: "#1976d2", marginLeft: 120 }}>
+          <Typography
+            variant="h5"
+            gutterBottom
+            sx={{ mt: 5, letterSpacing: 2 }}
+          >
+            <span style={{ fontWeight: "bold" }}>Programming Languages: </span>
+            JavaScript | Python | Java | HTML5 | CSS3 | SQL | Shell
+          </Typography>
+          <Typography
+            variant="h5"
+            gutterBottom
+            sx={{ mt: 5, letterSpacing: 2 }}
+          >
+            <span style={{ fontWeight: "bold" }}>Database: </span> MySQL |
+            MongoDB | CouchDB
+          </Typography>
+          <Typography
+            variant="h5"
+            gutterBottom
+            sx={{ mt: 5, letterSpacing: 2 }}
+          >
+            <span style={{ fontWeight: "bold" }}>Frameworks: </span> React |
+            Bootstrap | Express | Django
+          </Typography>
+          <Typography
+            variant="h5"
+            gutterBottom
+            sx={{ mt: 5, letterSpacing: 2 }}
+          >
+            <span style={{ fontWeight: "bold" }}>Developer Tools: </span> Git |
+            Linux | Ansible | Docker | AWS | Postman
+          </Typography>
+        </div>
+      </Grid>
+    </Grid>
   );
 }
