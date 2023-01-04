6f0ae352aa07f2faf635a8624b865d9aef998b92
ethangeau <yebing@student.unimelb.edu.au>
2023-01-04 23:39:00 +1100

    add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import NavBar from "./components/NavBar";
 import About from "./components/About";
 import Experience from "./components/Experience/Experience";
 import Portfolio from "./components/Portfolio/Portfolio";
@@ -9,6 +11,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
+      <NavBar />
       <About />
       <Experience />
       <Portfolio />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { navItems } from "../constants";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="flex justify-between p-2">
+      <h1>NavBar</h1>
+      <ul className="flex justify-center">
+        {navItems.map((item) => (
+          <li className=" px-2">{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default NavBar;
