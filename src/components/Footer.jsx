3c487d3489eda9ceacae3800e3acbc0af5cb5037
ethangeau <yebing@student.unimelb.edu.au>
2023-01-05 21:40:41 +1100

    update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <div className=" mt-40 mb-2 text-center">
+      <p>&copy; {year} Ethan Ge @ University of Melbourne</p>
+    </div>
+  );
 };
 
 export default Footer;
