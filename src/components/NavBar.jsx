95f43c4364a8fd7d1fc7796d8a6b957389574815
ethangeau <yebing@student.unimelb.edu.au>
2023-01-05 15:35:32 +1100

    change navItem name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import { Link } from "@mui/material";
 
-import { navItems } from "../constants";
+import { navItem } from "../constants";
 
 const NavBar = () => {
   return (
     <div className="flex justify-between py-10">
       <h1 className="text-xl">NavBar</h1>
       <ul className="flex justify-center">
-        {navItems.map((item) => (
+        {navItem.map((item) => (
           <li className="px-1 text-xl cursor-pointer md:px-2 lg:px-4">
             <Link
               href={`#${item.toLowerCase()}`}
