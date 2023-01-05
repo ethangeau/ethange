8e3790a65fbde4e1b726ac8be0194950a505c032
ethangeau <yebing@student.unimelb.edu.au>
2023-01-05 22:19:24 +1100

    update Navbar font

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 
 import { Link } from "@mui/material";
-
 import { navItem } from "../constants";
 
 const NavBar = () => {
   return (
     <div className="flex justify-between py-10">
-      <h1 className="text-xl">NavBar</h1>
+      <h1 className="text-xl font-medium">Ethan</h1>
       <ul className="flex justify-center">
         {navItem.map((item) => (
-          <li className="px-1 text-xl cursor-pointer md:px-2 lg:px-4">
+          <li className="px-1 text-xl cursor-pointer md:px-2 lg:px-4 font-medium">
             <Link
               href={`#${item.toLowerCase()}`}
               color="inherit"
