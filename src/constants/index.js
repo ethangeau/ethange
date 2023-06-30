8fa507522a0828a75ebfa42cb4c0cf23d53ebe46
ethangeau <yebing@student.unimelb.edu.au>
2023-06-30 20:04:21 +1000

    update Portfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,28 +3,49 @@ export const navItem = ["Portfolio", "Experience", "Skills", "Contact"];
 export const experienceItem = [
   {
     id: 1,
+    title: "Software Engineer",
+    company: "Job Search Ninja",
+    timeRanges: "Apr 2023 - Present",
+    location: "Melbourne, Australia",
+    website: "https://www.linkedin.com/company/job-search-ninja/",
+    description: [
+      "Developed core serverless features with Python and Firebase functions, improving system efficiency by 30%",
+      "Utilized OpenAI APIs for personalized resumes and cover letters, saving job seeks an estimated 5+ hours per week",
+    ],
+  },
+  {
+    id: 2,
+    title: "Full-Stack Engineer",
+    company: "Your ai (Closed)",
+    timeRanges: "Feb 2023 - Mar 2023",
+    location: "Melbourne, Australia",
+    website: "https://www.linkedin.com/company/your-ai/",
+    description: [
+      "Implemented 4 distinctive pages in a short period of time for the startup using React.js",
+      "Developed the entire backend features from scratch using Express.js, Node.js and OpenAI APIs",
+    ],
+  },
+  {
+    id: 3,
     title: "IT officer",
     company: "Strive Student Health Initiative Inc",
     timeRanges: "Oct 2022 - Present",
     location: "Melbourne, Australia",
     website: "https://strivehealth.org.au/",
     description: [
-      "Improved website design, consistency, and user experience, and increased its visibility and reach through SEO efforts",
-      "Maintained an organized online database to ensure easy access to key information",
+      "Managed and updated the organization's official website using WordPress, ensuring visibility through SEO efforts",
     ],
   },
   {
-    id: 2,
-    title: "Engineering Intern",
+    id: 4,
+    title: "Technology Intern",
     company: "Data Standards Body",
     timeRanges: "Jul 2022 - Oct 2022",
     location: "Melbourne, Australia",
-    website:
-      "https://www.directory.gov.au/portfolios/treasury/data-standards-body",
+    website: "https://www.linkedin.com/company/data-standards-body/",
     description: [
-      "Conducted research on tools in the market to automate the generation of behavior-driven test documentation",
-      "Collaborated in discussions and decision-making for the system architecture of the testdoc editor web application",
-      "Contributed to the frontend development of the testdoc editor web application using React",
+      "Collaborated in UI/UX design for an internal web tool, leading to enhanced workflow efficiency and user interaction",
+      "Contributed to frontend development with React and Material UI, enhancing interface usability and responsiveness",
     ],
   },
 ];
